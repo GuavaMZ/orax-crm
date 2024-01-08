ddd15f61a5163a7d2bf5252e21cd0f31b0aab7b1
GuavaMZ <guava.mzco@gmail.com>
2024-01-08 19:01:51 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/**/*.{html,js,ttf}","./**/*.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans:['Cairo'],
+    },
+  },
+  plugins: [],
+}
+
